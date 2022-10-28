2e371bf410c49f6c024d4cb748f57aa329d1fd68
gipoong <gipoong@naver.com>
2022-10-28 23:08:09 +0900

    2장, 3장, 4장

---
+++ chapter03/allow-21.js
@@ -0,0 +1,3 @@
+let total = (one, two) => one + two; // 줄을 분리하면 SyntaxError 발생
+let result = total(1, 2);
+console.log(result);
---
+++ chapter03/allow-32.js
@@ -0,0 +1,3 @@
+let get = param => ({sports:"축구"});
+let result = get();
+console.log(result);
